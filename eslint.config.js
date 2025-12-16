b7015e3ee46e42f0a12a2af4de822fe1b79e4fcd
Eric Jodet <eric_jodet@fr.ibm.com>
2025-12-16 10:41:21 +0100

    add eslint

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    ignores: ["**/test/**"],
+    rules: {
+      "no-unused-vars": "warn",
+      "no-useless-catch": "warn",
+      "no-case-declarations": "warn",
+      "no-useless-escape": "off",
+      "no-control-regex": "off",
+      "no-regex-spaces": "off",
+    },
+    plugins: {
+      js
+    },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        process: "readonly",
+        require: "readonly",
+      }
+    }
+  },
+]);
