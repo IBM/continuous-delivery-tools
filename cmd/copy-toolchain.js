baf57b897ec13c60b37158b9c960302978ee1274
Gigi Cheang <gigi.cheang@ibm.com>
2026-01-09 15:41:14 -0500

    docs: update copyright
    Signed-off-by: Gigi Cheang <gigi.cheang@ibm.com>

--- cmd/copy-toolchain.js
+++ cmd/copy-toolchain.js
@@ -1,6 +1,6 @@
 /**
  * Licensed Materials - Property of IBM
- * (c) Copyright IBM Corporation 2025. All Rights Reserved.
+ * (c) Copyright IBM Corporation 2025, 2026. All Rights Reserved.
  *
  * Note to U.S. Government Users Restricted Rights:
  * Use, duplication or disclosure restricted by GSA ADP Schedule
